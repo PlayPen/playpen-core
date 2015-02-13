dbb2261a4ce3ee4adce28aa0d357288c968a822e
Sam Bloomberg <sam@redxdev.com>
2015-02-12 19:21:49 -0500

    moving more stuff around

--- src/main/java/net/thechunk/playpen/coordinator/network/Network.java
+++ src/main/java/net/thechunk/playpen/coordinator/network/Network.java
@@ -8,11 +8,16 @@ import net.thechunk.playpen.Initialization;
 import net.thechunk.playpen.coordinator.PlayPen;
 import net.thechunk.playpen.networking.TransactionInfo;
 import net.thechunk.playpen.networking.TransactionManager;
+import net.thechunk.playpen.p3.P3Package;
 import net.thechunk.playpen.p3.PackageManager;
 import net.thechunk.playpen.protocol.Commands;
+import net.thechunk.playpen.protocol.P3;
 import net.thechunk.playpen.protocol.Protocol;
 import net.thechunk.playpen.utils.AuthUtils;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -114,5 +119,67 @@ public class Network extends PlayPen {
     @Override
     public boolean process(Commands.BaseCommand command, TransactionInfo info, String from) {
         return false;
+    }
+
+
+    public boolean sendPackage(String target, String transaction, String id, String version) {
+        P3Package p3 = getPackageManager().resolve(id, version);
+        if(p3 == null) {
+            log.error("Unable to send unresolved package " + id + " at " + version);
+            return false;
+        }
+
+        return sendPackage(target, transaction, p3);
+    }
+
+    public boolean sendPackage(String target, String transaction, P3Package p3) {
+        if(!p3.isResolved()) {
+            log.error("Cannot pass an unresolved package to sendPackage(target, p3)");
+            return false;
+        }
+
+        TransactionInfo info = TransactionManager.get().getInfo(transaction);
+        if(info == null) {
+            log.error("Unknown transaction " + transaction + ", unable to send package");
+            return false;
+        }
+
+        P3.P3Meta meta = P3.P3Meta.newBuilder()
+                .setId(p3.getId())
+                .setVersion(p3.getVersion())
+                .build();
+
+        byte[] packageBytes = null;
+        try {
+            packageBytes = Files.readAllBytes(Paths.get(p3.getLocalPath()));
+        }
+        catch(IOException e) {
+            log.error("Unable to read package data", e);
+            return false;
+        }
+
+        P3.PackageData data = P3.PackageData.newBuilder()
+                .setMeta(meta)
+                .setData(ByteString.copyFrom(packageBytes))
+                .build();
+
+        Commands.PackageResponse response = Commands.PackageResponse.newBuilder()
+                .setOk(true)
+                .setData(data)
+                .build();
+
+        Commands.BaseCommand command = Commands.BaseCommand.newBuilder()
+                .setType(Commands.BaseCommand.CommandType.PACKAGE_RESPONSE)
+                .setExtension(Commands.PackageResponse.command, response)
+                .build();
+
+        Protocol.Transaction message = TransactionManager.get()
+                .build(info.getId(), Protocol.Transaction.Mode.COMPLETE, command);
+        if(message == null) {
+            log.error("Unable to build transaction for package response");
+            return false;
+        }
+
+        return send(message, target);
     }
 }
