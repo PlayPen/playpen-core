7688262e4ec1912b267ab47afa8a265fc7b08356
Tux <write@imaginarycode.com>
2015-09-07 00:51:56 -0400

    FileProcessListener will now asynchronously write output.

--- src/main/java/net/thechunk/playpen/utils/process/FileProcessListener.java
+++ src/main/java/net/thechunk/playpen/utils/process/FileProcessListener.java
@@ -1,22 +1,24 @@
 package net.thechunk.playpen.utils.process;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class FileProcessListener implements IProcessListener {
-    private BufferedWriter writer = null;
+    private Writer writer = null;
+    private final ExecutorService service;
 
     public FileProcessListener(File file) throws IOException {
         DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss"); // quick, will probably change later
         Date date = new Date();
 
-        writer = Files.newBufferedWriter(file.toPath(), StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE);
+        writer = new OutputStreamWriter(Files.newOutputStream(file.toPath(), StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE));
+        service = Executors.newSingleThreadExecutor();
         writer.write("-- FileProcessListener SESSION STARTED " + dateFormat.format(date) + "\r\n");
         writer.flush();
     }
@@ -31,24 +33,33 @@ public class FileProcessListener implements IProcessListener {
 
     @Override
     public void onProcessOutput(XProcess proc, String out) {
-        try {
-            writer.write(out + "\n");
-            writer.flush();
+        if (!service.isShutdown()) {
+            service.execute(() -> {
+                try {
+                    writer.write(out);
+                    writer.write(System.lineSeparator());
+                } catch (IOException e) {
+                }
+            });
         }
-        catch(IOException e) {}
     }
 
     @Override
     public void onProcessInput(XProcess proc, String in) {
-        try {
-            writer.write(in);
-            writer.flush();
+        if (!service.isShutdown()) {
+            service.execute(() -> {
+                try {
+                    writer.write(in);
+                    writer.write(System.lineSeparator());
+                } catch (IOException e) {
+                }
+            });
         }
-        catch(IOException e) {}
     }
 
     @Override
     public void onProcessEnd(XProcess proc) {
+        service.shutdownNow();
         try {
             writer.write("-- FileProcessListener SESSION ENDED\r\n");
             writer.flush();
