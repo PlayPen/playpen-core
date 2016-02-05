6d2001bb5ebc738432949bd5274f309ce7e6a9f9
Tux <write@imaginarycode.com>
2016-02-05 00:24:30 -0500

    Read the full stream and use CRC32

--- src/main/java/io/playpen/core/utils/AuthUtils.java
+++ src/main/java/io/playpen/core/utils/AuthUtils.java
@@ -1,24 +1,20 @@
 package io.playpen.core.utils;
 
-import io.netty.buffer.ByteBuf;
+import com.google.common.io.ByteStreams;
 import io.playpen.core.protocol.Protocol;
 import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.io.output.NullOutputStream;
 import org.jasypt.encryption.pbe.StandardPBEByteEncryptor;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.zip.Adler32;
 import java.util.zip.CRC32;
 import java.util.zip.CheckedInputStream;
-import java.util.zip.Checksum;
 
 public class AuthUtils {
 
@@ -60,18 +56,8 @@ public class AuthUtils {
 
     public static String createPackageChecksum(String fp) throws IOException {
         Path path = Paths.get(fp);
-        try (CheckedInputStream is = new CheckedInputStream(Files.newInputStream(path), new Adler32())) {
-            byte[] buf = new byte[1024*1024];
-            int total = 0;
-            int c = 0;
-            while (total < 100*1024*1024 && (c = is.read(buf)) >= 0) {
-                total += c;
-            }
-
-            ByteBuffer bb = ByteBuffer.allocate(Long.BYTES);
-            bb.putLong(path.toFile().length());
-            buf = bb.array();
-            is.getChecksum().update(buf, 0, buf.length);
+        try (CheckedInputStream is = new CheckedInputStream(Files.newInputStream(path), new CRC32())) {
+            ByteStreams.copy(is, NullOutputStream.NULL_OUTPUT_STREAM);
             return Long.toHexString(is.getChecksum().getValue());
         }
     }
