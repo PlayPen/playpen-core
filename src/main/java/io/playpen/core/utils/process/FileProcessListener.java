f89360f2d77cf5e15c94a7a43b4c2ec684555cef
Tux <write@imaginarycode.com>
2016-11-04 23:04:55 -0400

    Improved FileProcessListener

--- src/main/java/io/playpen/core/utils/process/FileProcessListener.java
+++ src/main/java/io/playpen/core/utils/process/FileProcessListener.java
@@ -1,5 +1,7 @@
 package io.playpen.core.utils.process;
 
+import lombok.extern.log4j.Log4j2;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
@@ -8,21 +10,51 @@ import java.nio.file.StandardOpenOption;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
 
+@Log4j2
 public class FileProcessListener implements IProcessListener {
-    private BufferedWriter writer = null;
-    private final ExecutorService service;
+    private final BlockingDeque<String> messageQueue = new LinkedBlockingDeque<>();
+    private final Thread logWriter;
+    private volatile boolean stopped = false;
 
     public FileProcessListener(File file) throws IOException {
-        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss"); // quick, will probably change later
-        Date date = new Date();
+        logWriter = new Thread(() -> {
+            try (BufferedWriter writer = Files.newBufferedWriter(file.toPath(), StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE)) {
+                DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss"); // quick, will probably change later
+                writer.write("-- FileProcessListener SESSION STARTED " + dateFormat.format(new Date()) + "\r\n");
 
-        writer = Files.newBufferedWriter(file.toPath(), StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE);
-        service = Executors.newSingleThreadExecutor();
-        writer.write("-- FileProcessListener SESSION STARTED " + dateFormat.format(date) + "\r\n");
-        writer.flush();
+                while (!stopped) {
+                    try {
+                        String logEntry;
+                        while ((logEntry = messageQueue.take()) != null) {
+                            try {
+                                writer.write(logEntry);
+                                writer.write(System.lineSeparator());
+                                writer.flush();
+                            } catch (IOException e) {
+                                // We weren't able to flush the log entry to the disk. Wait a moment and then try again.
+                                Thread.sleep(250);
+                                messageQueue.addFirst(logEntry);
+                            }
+                        }
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().isInterrupted();
+                    }
+                }
+
+                for (String s : messageQueue) {
+                    writer.write(s);
+                    writer.write(System.lineSeparator());
+                }
+
+                writer.write("-- FileProcessListener SESSION ENDED " + dateFormat.format(new Date()) + "\r\n");
+            } catch (IOException e) {
+                log.error("Unable to write to log " + file, e);
+                stopped = true;
+            }
+        }, "PlayPen Logger - " + file.getName());
     }
 
     @Override
@@ -35,45 +67,21 @@ public class FileProcessListener implements IProcessListener {
 
     @Override
     public void onProcessOutput(XProcess proc, String out) {
-        if (!service.isShutdown()) {
-            service.execute(() -> {
-                try {
-                    writer.write(out);
-                    writer.write(System.lineSeparator());
-                    writer.flush();
-                } catch (IOException e) {
-                }
-            });
-        }
+        if (stopped) return;
+        messageQueue.add(out);
     }
 
     @Override
     public void onProcessInput(XProcess proc, String in) {
-        if (!service.isShutdown()) {
-            service.execute(() -> {
-                try {
-                    writer.write(in);
-                    writer.write(System.lineSeparator());
-                    writer.flush();
-                } catch (IOException e) {
-                }
-            });
-        }
+        if (stopped) return;
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss"); // quick, will probably change later
+        Date date = new Date();
+        messageQueue.add(dateFormat.format(date) + " - INPUT: " + in);
     }
 
     @Override
     public void onProcessEnd(XProcess proc) {
-        service.shutdownNow();
-        try {
-            writer.write("-- FileProcessListener SESSION ENDED\r\n");
-            writer.flush();
-        }
-        catch(IOException e) {}
-        finally {
-            try {
-                writer.close();
-            }
-            catch(IOException e) {}
-        }
+        stopped = true;
+        logWriter.interrupt();
     }
 }
--- src/main/java/io/playpen/core/utils/process/ProcessBuffer.java
+++ src/main/java/io/playpen/core/utils/process/ProcessBuffer.java
@@ -1,8 +1,8 @@
 package io.playpen.core.utils.process;
 
 import java.nio.CharBuffer;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * ProcessBuffer implements a way to turn {@code CharBuffers} from NuProcess into by-line output.
@@ -11,44 +11,51 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public abstract class ProcessBuffer {
     private CharBuffer rumpBuffer;
+    private final Object mutex = new Object();
 
-    public synchronized void append(CharBuffer buffer) {
-        CharBuffer combinedBuffer;
-        if (rumpBuffer != null) {
-            combinedBuffer = CharBuffer.allocate(rumpBuffer.length() + buffer.length());
-            combinedBuffer.put(rumpBuffer);
-            combinedBuffer.put(buffer);
-            combinedBuffer.flip();
-            rumpBuffer = null;
-        } else {
-            combinedBuffer = buffer;
-        }
+    public void append(CharBuffer buffer) {
+        List<String> lines = new ArrayList<>();
 
-        int lastNewline = 0;
-        boolean finishedLine = false;
-        for (int i = 0; i < combinedBuffer.remaining(); i++) {
-            char c = combinedBuffer.get(i);
-            finishedLine = false;
-            if (c == '\n' && i != combinedBuffer.length()) {
-                int previousLastNewline = lastNewline;
-                lastNewline = i + 1; // "Advance" the virtual position
-                if (previousLastNewline + 1 == i)
-                    continue;
+        synchronized (mutex) {
+            CharBuffer combinedBuffer;
+            if (rumpBuffer != null) {
+                combinedBuffer = CharBuffer.allocate(rumpBuffer.length() + buffer.length());
+                combinedBuffer.put(rumpBuffer);
+                combinedBuffer.put(buffer);
+                combinedBuffer.flip();
+                rumpBuffer = null;
+            } else {
+                combinedBuffer = buffer;
+            }
 
-                boolean hasR = combinedBuffer.length() > 1 && combinedBuffer.get(i - 1) == '\r';
-                CharBuffer sliced = (CharBuffer) combinedBuffer.slice().position(previousLastNewline).limit(i - (hasR ? 1 : 0));
-                onOutput(sliced.toString());
-                finishedLine = true;
+            int lastNewline = 0;
+            boolean finishedLine = false;
+            for (int i = 0; i < combinedBuffer.remaining(); i++) {
+                char c = combinedBuffer.get(i);
+                finishedLine = false;
+                if (c == '\n' && i != combinedBuffer.length()) {
+                    int previousLastNewline = lastNewline;
+                    lastNewline = i + 1; // "Advance" the virtual position
+                    if (previousLastNewline + 1 == i)
+                        continue;
+
+                    boolean hasR = combinedBuffer.length() > 1 && combinedBuffer.get(i - 1) == '\r';
+                    CharBuffer sliced = (CharBuffer) combinedBuffer.slice().position(previousLastNewline).limit(i - (hasR ? 1 : 0));
+                    lines.add(sliced.toString());
+                    finishedLine = true;
+                }
+            }
+
+            // Consume the buffer.
+            if (!finishedLine) {
+                this.rumpBuffer = CharBuffer.allocate(combinedBuffer.length() - lastNewline);
+                this.rumpBuffer.put((CharBuffer) combinedBuffer.slice().position(lastNewline).limit(combinedBuffer.length()));
+                this.rumpBuffer.flip();
             }
         }
 
-        // Consume the buffer.
-        if (!finishedLine) {
-            this.rumpBuffer = CharBuffer.allocate(combinedBuffer.length() - lastNewline);
-            this.rumpBuffer.put((CharBuffer) combinedBuffer.slice().position(lastNewline).limit(combinedBuffer.length()));
-            this.rumpBuffer.flip();
-        }
         buffer.position(buffer.remaining());
+        lines.forEach(this::onOutput);
     }
 
     protected abstract void onOutput(String output);
